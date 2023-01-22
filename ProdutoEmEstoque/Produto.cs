f70f5bc4347340cdbb1d6dab492a8d4f5bed566f
Bruno Duarte <duartebruno2208@gmail.com>
2023-01-22 14:13:26 -0300

    Add project files.

---
+++ ProdutoEmEstoque/Produto.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Globalization;
+
+namespace ProdutoEmEstoque
+{
+    internal class Produto
+    {
+        public string? Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade +=  quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return $"{Nome}, R$ {Preco.ToString("F2", CultureInfo.InvariantCulture)}, {Quantidade} unidades, Total: R$ {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
